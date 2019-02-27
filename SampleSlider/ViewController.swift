905999ffccb6b344f1b6cd7a56a8f17840c65145
daisukenagata <dbank0208@gmail.com>
2019-02-27 23:50:18 +0900

    Initial Commit

---
+++ SampleSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleSlider
+//
+//  Created by 永田大祐 on 2019/02/27.
+//  Copyright © 2019 永田大祐. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
